718aa43f9eaca4ccdefaa11aaeaf8a2e6a8f0db8
Kony Pham <kpham2@alumni.nd.edu>
2018-06-13 17:12:07 -0500

    Defined queue constructor for functional-shared instantiation.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  extend (storage, queueMethods);
+  return storage;
 };
 
-var queueMethods = {};
+var extend = function (obj, obj2) {
+  for (var key in obj2) {
+    obj[key] = obj2[key];
+  }
+};
+var queueMethods = {
+  size: function() {
+    return Object.keys(this).length - Object.keys(queueMethods).length;
+  },
+  enqueue: function(value) {
+    this[this.size()] = value;
+  },
+  dequeue: function() {
+    var result = this[0];
+    delete this[0];
+
+    for (var i = 1; i <= this.size(); i++) {
+      this [i - 1] = this[i];
+    }
+
+    delete this[this.size() - 1];
+    return result;
+  }
+};
 
 
