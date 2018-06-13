95a9647500caffdd25ada52aa5821ebd33b0853d
Kony Pham <kpham2@alumni.nd.edu>
2018-06-13 18:04:24 -0500

    Defined stack constructor for ES6 instantiation

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,21 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this;
+  }
+
+  size() {
+    return Object.keys(this).length;
+  }
+
+  push(value) {
+    this[this.size()] = value;
+  }
+
+  pop() {
+    var poppedItem = this[this.size() - 1];
+    delete this[this.size() - 1];
+    return poppedItem;
   }
 
 }
