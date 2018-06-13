fe4e08ee371643eeb322d1d918dbee364374c44b
Kony Pham <kpham2@alumni.nd.edu>
2018-06-13 17:35:28 -0500

    Defined stack constructor for prototypal instantiation

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = Object.create(stackMethods);
+  return storage;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function () {
+    return Object.keys(this).length;
+  },
+  push: function (value) {
+    this[this.size()] = value;
+  },
+  pop: function (value) {
+    var index = this.size() - 1;
+    var result = this[index];
+    delete this[index];
+    return result;
+  }
+};
 
 
