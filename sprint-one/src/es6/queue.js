a5b2990753824876ede20b780bc6f6ae6d9b972b
Kony Pham <kpham2@alumni.nd.edu>
2018-06-13 18:00:52 -0500

    Defined queue constructor for ES6 instnatiation

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this;
+  }
+
+  size() {
+    return Object.keys(this).length;
+  }
+
+  enqueue(value) {
+    this[this.size()] = value;
+  }
+
+  dequeue() {
+    var result = this[0];
+    delete this[0];
+
+    for (var i = 1; i <= this.size(); i++) {
+      this [i - 1] = this[i];
+    }
+
+    delete this[this.size() - 1];
+    return result;
   }
 
 }
