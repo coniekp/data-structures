c5cb447f6f97ee3ec6163a44b65bf5b97fe1e506
Kony Pham <kpham2@alumni.nd.edu>
2018-06-13 17:50:15 -0500

    Defined stack constructor for pseudoclassical instantiation

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this;
+};
+
+Stack.prototype.size = function () {
+  return Object.keys(this).length;
+};
+
+Stack.prototype.push = function (value) {
+  this[this.size()] = value;
+};
+
+Stack.prototype.pop = function () {
+  var poppedItem = this[this.size() - 1];
+  delete this[this.size() - 1];
+  return poppedItem;
 };
 
 
