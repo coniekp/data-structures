74012f1883befe3548031a2b29f0529dfb18f5fd
Kony Pham <kpham2@alumni.nd.edu>
2018-06-13 17:47:10 -0500

    Defined queue constructor for psuedoclassical instantiation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this;
 };
 
+Queue.prototype.size = function () {
+  return Object.keys(this).length;
+};
+
+Queue.prototype.enqueue = function (value) {
+  this[this.size()] = value;
+};
+
+Queue.prototype.dequeue = function () {
+  var dequeuedItem = this[0];
+  delete this[0];
+  for (var i = 1; i <= this.size(); i++) {
+    this[i - 1] = this[i];
+  }
+  delete this[this.size() - 1];
+  return dequeuedItem;
+};
 
