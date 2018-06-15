f742ec69216a40cfc314f48d205ee70e96ccf872
coniekp <coniekp@icloud.com>
2018-06-15 15:16:17 -0700

    Defined hashTable constructor

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,4 @@
 
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +6,56 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var hasNotBeenInserted = true;
+  var count = 0;
+  var storage = this._storage;
+  
+  while (count < this._limit && hasNotBeenInserted) {
+    if (!storage[index]) { 
+    //slot is empty, insert
+      storage[index] = {};
+      storage[index][k] = v;
+      hasNotBeenInserted = false;
+      
+    } else {
+      if (!storage[index][k]) {
+      //if slot has a property but that is not k >> [COLLISON]
+        index = (index + 1) % this._limit;
+        count++;
+      } else {
+      //if slot has k, overwrite
+        storage[index][k] = v;
+        hasNotBeenInserted = false;
+      }  
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var count = 0;
+  var storage = this._storage;
+  
+  while (count < this._limit) {
+    if (!storage[index] || !storage[index][k]) {
+      //loop through storage if slot is empty or if collision
+      index = (index + 1) % this._limit;
+    } else {
+      return storage[index][k];
+    }
+    count++;
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var storage = this._storage;
+  while (!storage[index][k]) {
+    index = (index + 1) % this._limit;
+  }
+  storage[index] = undefined;
 };
 
 
@@ -22,5 +63,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
