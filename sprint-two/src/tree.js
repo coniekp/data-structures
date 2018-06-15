69f1779c3635f900dc62893c1bbc2b9adb377f7c
coniekp <coniekp@icloud.com>
2018-06-15 15:25:02 -0700

    Defined tree constructor

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,19 +2,38 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newTree = Tree(value);
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
+ 
+  var searchChildren = function(tree) {
+    /* check whether the curtree has the target if does return true,
+     else call searchChildren on its children */
+    if (tree.value === target) { return true; } else {
+      
+      var hasTarget = false;
+      
+      for (var i = 0; i < tree.children.length; i++) {
+        hasTarget = hasTarget || searchChildren(tree.children[i]);
+      }
+      
+      return hasTarget;
+    }   
+  };
+ 
+  return searchChildren(this);
 };
+
 
 
 
