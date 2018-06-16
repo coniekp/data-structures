791903a7e8985e0e414f6a7677f3aeefdfd90590
coniekp <coniekp@icloud.com>
2018-06-15 17:30:33 -0700

    Amended callback argument to node.value (previously node) in .forEachNode()

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -88,7 +88,7 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
   for (var i = 0; i < this.nodes.length; i++) {
-    cb (this.nodes[i]);
+    cb (this.nodes[i].value);
   }
 };
 
