3148cd020a5f16f41fe19f0689af2d7dda0a6dfe
coniekp <coniekp@icloud.com>
2018-06-15 17:41:23 -0700

    Defined linkedList constructor

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-//testing
+    var node = Node(value);
+    if (!list.head && !list.tail) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var removedHead = list.head;
+    if (list.head) {
+      list.head = list.head.next;
+    }
+    return removedHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -24,6 +45,10 @@ var Node = function(value) {
 
   return node;
 };
+
+/*
+* Complexity: What is the time complexity of the above functions?
+*/
 
 /*
  * Complexity: What is the time complexity of the above functions?
